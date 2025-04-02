f911f990760b48fdc4da22bef98b51ee38eb0493
Jaideep-Holbox <jaideep.dabhi@holbox.ai>
2025-04-02 16:12:55 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md","r") as f :
 
 setup(
     name="newberryai",
-    version="0.1.0",
+    version="0.1.1",
     author="Saurabh Patil, Jaideepsinh Dabhi",
     author_email="jaideep@newberry.ai",
     description="NewberryAI Python Package",
