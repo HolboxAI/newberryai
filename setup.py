d370911cb3e0cbf058d16674bc192ff080e1ad73
Jaideep-Holbox <jaideep.dabhi@holbox.ai>
2025-04-02 15:38:52 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,6 +22,14 @@ setup(
             "newberryai=newberryai.cli:main",
         ]
     },
+    url="https://github.com/HolboxAI/newberryai",
     long_description=description,
     long_description_content_type="text/markdown",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Operating System:: OS Independent",
+    ],
+    python_requires=">=3.6",
 )
